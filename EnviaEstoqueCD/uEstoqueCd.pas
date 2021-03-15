33b843f14e6a7338d3f3e5de7beddaac3d6098b6
BrunoAquino2006 <80278676+BrunoAquino2006@users.noreply.github.com>
2021-03-15 09:32:49 -0300

    Foi trocado o select e adicionado mais 3 campos.

--- EnviaEstoqueCD/uEstoqueCd.pas
+++ EnviaEstoqueCD/uEstoqueCd.pas
@@ -8,11 +8,9 @@ uses
   FireDAC.Stan.Error, FireDAC.UI.Intf, FireDAC.Phys.Intf, FireDAC.Stan.Def,
   FireDAC.Stan.Pool, FireDAC.Stan.Async, FireDAC.Phys, FireDAC.Phys.Oracle,
   FireDAC.Phys.OracleDef, FireDAC.VCLUI.Wait, Data.DB, FireDAC.Comp.Client,
-  FireDAC.Stan.Param, FireDAC.DatS, FireDAC.DApt.Intf, FireDAC.DApt,
-  FireDAC.Comp.DataSet, ppDesignLayer, ppParameter, ppDB, ppDBPipe, ppModule,
-  daDataModule, ppBands, ppClass, ppCtrls, ppStrtch, ppMemo, ppVar, ppPrnabl,
-  ppCache, ppComm, ppRelatv, ppProd, ppReport, Vcl.StdCtrls, Vcl.Mask,
-  Vcl.Buttons, ACBrBase, ACBrMail,System.DateUtils,System.IniFiles;
+  FireDAC.Stan.Param,  Vcl.StdCtrls, Vcl.Mask,
+  Vcl.Buttons, ACBrBase, ACBrMail,System.DateUtils,System.IniFiles,
+  FireDAC.DatS, FireDAC.DApt.Intf, FireDAC.DApt, FireDAC.Comp.DataSet;
 
 type
   TfrmEstoqueCD = class(TForm)
@@ -54,46 +52,70 @@ uses uFunc, ufuncoes;
 procedure TfrmEstoqueCD.btnImprimirClick(Sender: TObject);
 var
 sCaminhoArquivo,sNomeArquivo,sCabecalho,sHtml,sPerUso,sNumSorteUsado,sNumSorteDisponivel : String;
-sValor,sPecas,sCodigos :string;
+sValor,sPecas,sCodigos, sCodigosVDA, sQtdVDA, sValorVDA :string;
+codEstoque,codEstoqueRes, qtdEstoque, qtdEstoqueRes, valorEstoque, valorEstoqueRes, codVda, codVdaRes, qtdVda, qtdVdaRes, valorVda, valorVdaRes : integer;
 begin
         sCaminhoArquivo := 'c:\EstoqueCD\';
 
     sCabecalho := '';
     sCabecalho := sCabecalho + '<tr>';
     sCabecalho := sCabecalho + '<th>Cod. Grupos</th>';
-    sCabecalho := sCabecalho + '<th>Qtd. Codigos</th>';
-    sCabecalho := sCabecalho + '<th>Qtd. Peças</th>';
-    sCabecalho := sCabecalho + '<th>Valor</th>';
+    sCabecalho := sCabecalho + '<th>Qtd. Codigos Estoque</th>';
+    sCabecalho := sCabecalho + '<th>Qtd. Peças Estoque</th>';
+    sCabecalho := sCabecalho + '<th>Valor Estoque</th>';
+    sCabecalho := sCabecalho + '<th>Qtd. Cód. Barras Venda</th>';
+    sCabecalho := sCabecalho + '<th>Qtd. Venda Semanal</th>';
+    sCabecalho := sCabecalho + '<th>Valor Venda Semanal</th>';
     sCabecalho := sCabecalho + '</tr>';
 
     qry.Active := false;
     qry.SQL.Clear;
-    qry.sql.Add(' select est.cod_unidade                     '+
-                ' ,count(distinct est.cod_item) nro_skus     '+
-                ' ,sum(est.qtde) qtde                        '+
-                ' ,sum(est.valor) valor                      '+
-                ' from                                       '+
-                ' (select a.cod_unidade                      '+
-                ' ,a.cod_item                                '+
-                ' ,(nvl(a.qtd_estoque,0)) qtde               '+
-                ' ,(nvl(a.vlr_medio_total,0)) valor          '+
-                ' from ce_estoques a                         '+
-                ' ,ie_itens b                                '+
-                ' ,ie_mascaras c                             '+
-                ' where c.cod_item=a.cod_item                '+
-                ' and c.cod_mascara=150                      '+
-                ' and c.cod_niv0=1                           '+
-                ' and b.cod_item=a.cod_item                  '+
-                ' and b.ind_avulso=0                         '+
-                ' and b.ind_inativo=0                        '+
-                ' and a.cod_emp=1                            '+
-                ' and nvl(a.qtd_estoque,0) > 0               '+
-                ' and a.cod_unidade in (818,838,848,858)     '+
-                '  ) est                                     '+
-                ' where est.qtde > 0                         '+
-                ' group by est.cod_unidade                   '+
-                ' order by est.cod_unidade                   ');
-
+    qry.sql.Add('select est.cod_unidade                                                                               '+
+                ',est.nro_skus nro_skus_est                                                                           '+
+                ',est.qtde qtde_est                                                                                   '+
+                ',est.valor valor_est                                                                                 '+
+                ',ce_dia.nro_skus nro_skus_vda                                                                        '+
+                ',ce_dia.qtde qtde_vda                                                                                '+
+                ',ce_dia.valor valor_vda                                                                              '+
+                'from                                                                                                 '+
+                '(select a.cod_unidade                                                                                '+
+                ',count(distinct a.cod_item) nro_skus                                                                 '+
+                ',sum(nvl(a.qtd_estoque,0)) qtde                                                                    '+
+                ',sum(nvl(a.vlr_medio_total,0)) valor                                                               '+
+                'from ce_estoques a                                                                                   '+
+                ',ie_itens b                                                                                          '+
+                'where b.cod_item=a.cod_item                                                                          '+
+                'and b.ind_avulso=0                                                                                   '+
+                'and b.ind_inativo=0                                                                                  '+
+                'and b.cod_tipo=''00''                                                                                  '+
+                'and a.cod_emp=1                                                                                      '+
+                'and nvl(a.qtd_estoque,0) > 0                                                                         '+
+                'and a.cod_unidade in (818,838,848,858)                                                               '+
+                'group by a.cod_unidade                                                                               '+
+                ' ) est                                                                                               '+
+                ',(select substr(to_char(ge.cod_nivel2),1,2)||''8'' cod_unidade                                         '+
+                ' ,count(distinct ce.cod_item) nro_skus                                                               '+
+                ',sum(decode(ce.tip_lancamento,2,nvl(ce.qtd_lancamento,0),(nvl(ce.qtd_lancamento,0) * -1))) qtde      '+
+                ',sum(decode(ce.tip_lancamento,2,nvl(ce.vlr_total,0),(nvl(ce.vlr_total,0) * -1))) valor            '+
+                'from ce_diarios ce                                                                                   '+
+                ',ie_itens ie                                                                                         '+
+                ',ge_unidades ge                                                                                      '+
+                'where ge.cod_unidade=ce.cod_unidade                                                                  '+
+                'and ie.cod_item=ce.cod_item                                                                          '+
+                'and ie.ind_avulso=0                                                                                  '+
+                'and ie.ind_inativo=0                                                                                 '+
+                'and ie.cod_tipo=''00''                                                                                 '+
+                'and ce.cod_emp=1                                                                                     '+
+                'and ce.ind_estoque=1                                                                                 '+
+                'and ce.tip_lancamento in (1,2)                                                                       '+
+                'and ce.ind_venda=1                                                                                   '+
+                'and ce.cod_oper in (106,107,300,302,305,2106,2107,3300,3302,3305)                                    '+
+                'and ce.dta_lancamento >= trunc(sysdate - 7)                                                          '+
+                'and ce.dta_lancamento <  trunc(sysdate)                                                              '+
+                'group by ge.cod_nivel2                                                                               '+
+                ' ) ce_dia                                                                                            '+
+                'where est.cod_unidade = ce_dia.cod_unidade                                                           '+
+                'order by est.cod_unidade                                                                             ');
     qry.Active := true;
 
 
@@ -115,52 +137,53 @@ begin
            qry.First;
            while not qry.Eof do
             Begin
+                codEstoque :=  qry.FieldByName('nro_skus_est').AsInteger;
+                codEstoqueRes := codEstoqueRes + codEstoque;
+                qtdEstoque := qry.FieldByName('qtde_est').AsInteger;
+                qtdEstoqueRes := qtdEstoqueRes + qtdEstoque;
+                valorEstoque := qry.FieldByName('valor_est').AsInteger;
+                valorEstoqueRes := valorEstoqueRes + valorEstoque;
+                codVda := qry.FieldByName('nro_skus_vda').AsInteger;
+                codVdaRes := codVdaRes + codVda;
+                qtdVda := qry.FieldByName('qtde_vda').AsInteger;
+                qtdVdaRes := qtdVdaRes + qtdVda;
+                valorVda := qry.FieldByName('valor_vda').AsInteger;
+                valorVdaRes := valorVdaRes + valorVda;
 
                 sHtml := sHtml + '<tr>';
                 sHtml := sHtml + '<td align="right" WIDTH=100>'+qry.FieldByName('cod_unidade').AsString+'</td>';
-                sHtml := sHtml + '<td align="right" WIDTH=100>'+FormatFloat('#,###',qry.FieldByName('nro_skus').AsInteger)+'</td>';
-                sHtml := sHtml + '<td align="right" WIDTH=100>'+FormatFloat('#,###',qry.FieldByName('qtde').AsInteger)+'</td>';
-                sHtml := sHtml + '<td align="right" WIDTH=100>'+FormatFloat('#,0.00;-#,0.00',qry.FieldByName('valor').AsFloat)+'</td>';
+                sHtml := sHtml + '<td align="right" WIDTH=100>'+FormatFloat('#,###',qry.FieldByName('nro_skus_est').AsInteger)+'</td>';
+                sHtml := sHtml + '<td align="right" WIDTH=100>'+FormatFloat('#,###',qry.FieldByName('qtde_est').AsInteger)+'</td>';
+                sHtml := sHtml + '<td align="right" WIDTH=100>'+FormatFloat('#,0.00;-#,0.00',qry.FieldByName('valor_est').AsFloat)+'</td>';
+                sHtml := sHtml + '<td align="right" WIDTH=100>'+FormatFloat('#,###',qry.FieldByName('nro_skus_vda').AsInteger)+'</td>';
+                sHtml := sHtml + '<td align="right" WIDTH=100>'+FormatFloat('#,###',qry.FieldByName('qtde_vda').AsInteger)+'</td>';
+                sHtml := sHtml + '<td align="right" WIDTH=100>'+FormatFloat('#,0.00;-#,0.00',qry.FieldByName('valor_vda').AsFloat)+'</td>';
                 sHtml := sHtml + '</tr>';
                 qry.Next;
 
             end;
-           qry.Close;
-           FDQuery1.Active := false;
-           FDQuery1.SQL.Clear;
-           FDQuery1.sql.Add('select                    '+
-                        ' count(distinct est.cod_item) nro_skus     '+
-                        ' ,sum(est.qtde) qtde                        '+
-                        ' ,sum(est.valor) valor                      '+
-                        ' from                                       '+
-                        ' (select a.cod_unidade                      '+
-                        ' ,a.cod_item                                '+
-                        ' ,(nvl(a.qtd_estoque,0)) qtde               '+
-                        ' ,(nvl(a.vlr_medio_total,0)) valor          '+
-                        ' from ce_estoques a                         '+
-                        ' ,ie_itens b                                '+
-                        ' ,ie_mascaras c                             '+
-                        ' where c.cod_item=a.cod_item                '+
-                        ' and c.cod_mascara=150                      '+
-                        ' and c.cod_niv0=1                        '+
-                        ' and b.cod_item=a.cod_item                  '+
-                        ' and b.ind_avulso=0                         '+
-                        ' and b.ind_inativo=0                        '+
-                        ' and a.cod_emp=1                            '+
-                        ' and nvl(a.qtd_estoque,0) > 0               '+
-                        ' and a.cod_unidade in (818,838,848,858)     '+
-                        '  ) est                                     '+
-                        ' where est.qtde > 0 ');
 
-            FDQuery1.Active := true;
-            sValor    := FormatFloat('#,0.00;-#,0.00',fDQuery1.FieldByName('valor').AsFloat);
-            sPecas    := FormatFloat('#,###',  fDQuery1.FieldByName('qtde').AsInteger);
-            sCodigos  := FormatFloat('#,###',  fDQuery1.FieldByName('nro_skus').AsInteger);
+
+
+          //  sValor    := FormatFloat('#,0.00;-#,0.00',fDQuery1.FieldByName('valor_est').AsFloat);
+           // sPecas    := FormatFloat('#,###',  fDQuery1.FieldByName('qtde_est').AsInteger);
+            sCodigos  := FormatFloat('#,###', codEstoqueRes);
+            sPecas  := FormatFloat('#,###', qtdEstoqueRes);
+            sValor  := FormatFloat('#,0.00;-#,0.00', valorEstoqueRes);
+            sCodigosVDA  := FormatFloat('#,###', codVdaRes);
+            sQtdVDA  := FormatFloat('#,###', qtdVdaRes);
+            sValorVDA  := FormatFloat('#,0.00;-#,0.00', valorVdaRes);
+
+
+
                 sHtml := sHtml + '<tr>';
                 sHtml := sHtml + '<td align="right" <th>TOTAL</th>';
                 sHtml := sHtml + '<td align="right" WIDTH=100>'+sCodigos+'</td>';
                 sHtml := sHtml + '<td align="right" WIDTH=100>'+sPecas+'</td>';
-                sHtml := sHtml + '<td align="right" WIDTH=100>'+svalor+'</td>';
+                sHtml := sHtml + '<td align="right" WIDTH=100>'+sValor+'</td>';
+                sHtml := sHtml + '<td align="right" WIDTH=100>'+sCodigosVDA+'</td>';
+                sHtml := sHtml + '<td align="right" WIDTH=100>'+sQtdVDA+'</td>';
+                sHtml := sHtml + '<td align="right" WIDTH=100>'+sValorVDA+'</td>';
                 sHtml := sHtml + '</tr>';
        end
 
